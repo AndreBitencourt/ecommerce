56bf255d72afc47cc65469221635ace5b64909f1
Andre da Silva Bitencourt <andredasilvabitencourt@outlook.com>
2018-04-04 14:03:32 -0300

    Criamos o home page

--- index.php
+++ index.php
@@ -2,19 +2,29 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();//rotas
+
+//$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql;
+    $page = new Page();
+    
+    $page->setTpl("index");
+    
+    
+	/*$sql = new Hcode\DB\Sql;
         $results = $sql->select("SELECT * FROM tb_users");
         
-        echo json_encode($results);
+        echo json_encode($results);*/
 
 });
 
-$app->run();
+$app->run();//Faz rodar
 
  ?>
